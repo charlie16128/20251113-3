ba1e136dd5e5722cf9a3cd8a45d2e6f3b7e32148
charlie16128 <ctugm24@gmail.com>
2025-11-13 17:36:36 +0800

    first test

---
+++ TicTacToe.java
@@ -0,0 +1,73 @@
+﻿public class TicTacToe {
+    private char[][] board;
+    private int moves;
+
+    public TicTacToe() {
+        board = new char[3][3];
+        for (int i = 0; i < 3; i++) {
+            for (int j = 0; j < 3; j++) {
+                board[i][j] = ' ';
+            }
+        }
+        moves = 0;
+    }
+
+    // 玩家下棋
+    public boolean set(int row, int col, char player) {
+        if (row < 0 || row > 2 || col < 0 || col > 2) {
+            return false;
+        }
+        if (player != 'O' && player != 'X') {
+            return false;
+        }
+        if (board[row][col] != ' ') {
+            return false; // 已經有棋子
+        }
+
+        board[row][col] = player;
+        moves++;
+        return true;
+    }
+
+    // 判斷勝負
+    public String evaluate() {
+        // 判斷橫排與直排
+        for (int i = 0; i < 3; i++) {
+            // 橫排
+            if (board[i][0] != ' ' &&
+                board[i][0] == board[i][1] &&
+                board[i][1] == board[i][2]) {
+                return String.valueOf(board[i][0]);
+            }
+
+            // 直排
+            if (board[0][i] != ' ' &&
+                board[0][i] == board[1][i] &&
+                board[1][i] == board[2][i]) {
+                return String.valueOf(board[0][i]);
+            }
+        }
+
+        // 判斷斜線
+        if (board[1][1] != ' ') {
+            if (board[0][0] == board[1][1] && board[1][1] == board[2][2]) {
+                return String.valueOf(board[1][1]);
+            }
+            if (board[0][2] == board[1][1] && board[1][1] == board[2][0]) {
+                return String.valueOf(board[1][1]);
+            }
+        }
+
+        // 平手
+        if (moves == 9) {
+            return "Draw";
+        }
+
+        return "Pending"; // 尚未結束
+    }
+
+    // 提供給測試使用
+    public char get(int row, int col) {
+        return board[row][col];
+    }
+}
---
+++ TicTacToeTest.java
@@ -0,0 +1,49 @@
+﻿import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.Test;
+
+public class TicTacToeTest {
+
+    @Test
+    void testOWin() {
+        TicTacToe game = new TicTacToe();
+        game.set(0, 0, 'O');
+        game.set(1, 0, 'O');
+        game.set(2, 0, 'O');
+
+        assertEquals("O", game.evaluate());
+    }
+
+    @Test
+    void testXWinDiagonal() {
+        TicTacToe game = new TicTacToe();
+        game.set(0, 0, 'X');
+        game.set(1, 1, 'X');
+        game.set(2, 2, 'X');
+
+        assertEquals("X", game.evaluate());
+    }
+
+    @Test
+    void testDraw() {
+        TicTacToe game = new TicTacToe();
+        game.set(0,0,'X');
+        game.set(0,1,'O');
+        game.set(0,2,'X');
+        game.set(1,0,'O');
+        game.set(1,1,'X');
+        game.set(1,2,'O');
+        game.set(2,0,'O');
+        game.set(2,1,'X');
+        game.set(2,2,'O');
+
+        assertEquals("Draw", game.evaluate());
+    }
+
+    @Test
+    void testPending() {
+        TicTacToe game = new TicTacToe();
+        game.set(0,0,'X');
+
+        assertEquals("Pending", game.evaluate());
+    }
+}
